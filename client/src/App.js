24aa8222ccc2fca4bbb1b51d4095c7812e290499
[Shaikh Faruk] <[sfaruk1137@gmail.com]>
2021-09-08 18:46:46 +0530

    Faruk: Server and Clients folder added

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Advices from "./pages/Advices";
+import Chats from "./pages/Chats";
+import DoctorProfile from "./pages/DoctorProfile";
+import UserProfile from "./pages/UserProfile";
+import Services from "./pages/Services";
+import Setting from "./pages/Setting";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/about">
+          <About />
+        </Route>
+
+        <Route exact path="/advices">
+          <Advices />
+        </Route>
+
+        <Route exact path="/chats">
+          <Chats />
+        </Route>
+
+        <Route exact path="/doctorprofile">
+          <DoctorProfile />
+        </Route>
+
+        <Route exact path="/userprofile">
+          <UserProfile />
+        </Route>
+
+        <Route exact path="/services">
+          <Services />
+        </Route>
+
+        <Route exact path="/setting">
+          <Setting />
+        </Route>
+
+        <Route exact path="/login">
+          <Login />
+        </Route>
+
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>hello navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
