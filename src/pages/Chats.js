acd4e0f5a2ffbf0b48d31d0db2286e43b5fe2628
[Shaikh Faruk] <[sfaruk1137@gmail.com]>
2021-09-08 18:15:22 +0530

    Faruk:- Frist setup Complate

---
+++ src/pages/Chats.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chats = () => {
+  return (
+    <div>
+      <h1>chats</h1>
+    </div>
+  );
+};
+
+export default Chats;
---
+++ src/pages/DoctorProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DoctorProfile = () => {
+  return (
+    <div>
+      <h1>DoctorProfile</h1>
+    </div>
+  );
+};
+
+export default DoctorProfile;
