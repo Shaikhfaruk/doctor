148f5c15e2d7aa6ee48a8bfe189286feeff70126
[Shaikh Faruk] <[sfaruk1137@gmail.com]>
2021-09-09 14:56:13 +0530

    Faruk: Basic Navbar Done

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,50 +5,46 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="logo">
-        <Link to="/">Home</Link>
+        <Link to="/">Doctor</Link>
       </div>
-      <div className="search">
+      {/* <div className="search">
         <input type="search" name="search" id="searech" />
-      </div>
+      </div> */}
 
-      <div className="navlinks">
-        <ul>
-          <li className="nav-items">
-            <Link to="/about" className="nav-item">
-              About
-            </Link>
-          </li>
+      <ul className="nav-items">
+        <li className="nav-item">
+          <Link to="/about" className="nav-item">
+            About
+          </Link>
+        </li>
 
-          <li className="nav-items">
-            <Link to="/advices" className="nav-item">
-              Advices
-            </Link>
-          </li>
+        <li className="nav-item">
+          <Link to="/advices" className="nav-item">
+            Advices
+          </Link>
+        </li>
 
-          <li className="nav-items">
-            <Link to="/services" className="nav-item">
-              Services
-            </Link>
-          </li>
+        <li className="nav-item">
+          <Link to="/services" className="nav-item">
+            Services
+          </Link>
+        </li>
 
-          <li className="nav-items">
-            <Link to="/chats" className="nav-item">
-              Chats
-            </Link>
-          </li>
+        <li className="nav-item">
+          <Link to="/chats" className="nav-item">
+            Chats
+          </Link>
+        </li>
+      </ul>
 
-          <li className="nav-items">
-            <Link to="/login" className="nav-item">
-              Login
-            </Link>
-          </li>
+      <div className="loginbtn">
+        <button className="btn">
+          <Link to="/login">Login</Link>
+        </button>
 
-          <li className="nav-items">
-            <Link to="/signup" className="nav-item">
-              Sign Up
-            </Link>
-          </li>
-        </ul>
+        <button className="btn">
+          <Link to="/signup">Signup</Link>
+        </button>
       </div>
     </div>
   );
